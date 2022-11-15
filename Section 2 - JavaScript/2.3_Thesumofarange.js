633e4bf672b5a58c9e913d8467fdfc60ec43cff8
z1OHH <oh.weerapat@gmail.com>
2022-11-15 22:15:13 +0700

    -

--- Section 2 - JavaScript/2.3_Thesumofarange.js
+++ Section 2 - JavaScript/2.3_Thesumofarange.js
@@ -37,5 +37,5 @@ function sum(num) {
 console.log(range(1, 10));
 console.log(range(5, 2));
 console.log(range(1, 10, 2));
-console.log(range(8, 2, -3));
+console.log(range(8, 2, -2));
 console.log(sum(range(1, 10)));
