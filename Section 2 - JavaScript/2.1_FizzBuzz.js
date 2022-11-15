02aabb0085716e244ff69c1e9612ab4518b1f111
z1OHH <oh.weerapat@gmail.com>
2022-11-15 16:23:22 +0700

    Fix 1

--- Section 2 - JavaScript/2.1_FizzBuzz.js
+++ Section 2 - JavaScript/2.1_FizzBuzz.js
@@ -1,14 +1,14 @@
 for(let i = 1; i <= 100;i++){
-    if( i % 3 == 0 ) {
-        console.log("Fizz")
-    }
-    if( i % 5 == 0 ) {
-        console.log("Buzz")
-    }
     if( i % 3 == 0 && i % 5 == 0) {
         console.log("FizzBuzz")
     }
-    if( i % 3 != 0 && i % 5 != 0) {
+    else if( i % 3 == 0 ) {
+        console.log("Fizz")
+    }
+    else if( i % 5 == 0 ) {
+        console.log("Buzz")
+    }
+    else {
         console.log(i)
     }
 }
