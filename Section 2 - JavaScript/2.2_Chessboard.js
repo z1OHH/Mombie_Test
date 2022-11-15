490f2b8994678be50a079396ab54ac2e2aa43f68
z1OHH <oh.weerapat@gmail.com>
2022-11-15 17:37:41 +0700

    Complete

---
+++ Section 2 - JavaScript/2.2_Chessboard.js
@@ -0,0 +1,12 @@
+let cboard = "", height = 8, width = 8
+for (let i = 1; i <= height; i++) {
+  for (let j = 1; j <= width; j++) {
+    if ((i + j) % 2 == 0) { 
+        cboard += " ";
+    } else {
+        cboard += "#";
+    }
+  }
+  cboard += "\n";
+}
+console.log(cboard);
