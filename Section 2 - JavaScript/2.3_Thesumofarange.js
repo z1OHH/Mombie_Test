b427316b2f82f27c44d574aeb414bfd5ce2bea11
z1OHH <oh.weerapat@gmail.com>
2022-11-15 21:31:15 +0700

    -

--- Section 2 - JavaScript/2.3_Thesumofarange.js
+++ Section 2 - JavaScript/2.3_Thesumofarange.js
@@ -1,21 +1,26 @@
 function range(start,end){
-let rangenum = []
-if(start < end){
-    for(let i = start; i <= end;i++){
-        rangenum.push(i)
+    let rangenum = []
+    if(start < end){
+        for(let i = start; i <= end;i++){
+            rangenum.push(i)
+        }
+        return rangenum
+    }
+    else{
+        for(let i = start; i >= end;i--){
+            rangenum.push(i)
+        }
+        return rangenum
     }
 }
-else{
-    for(let i = start; i >= end;i--){
-        rangenum.push(i)
-    }
-}
-return rangenum
-}
-
-function sum(num){
     
+function sum(num){
+    total=0
+    for (let i = 1; i <= num.length; i++) {
+        total+=i
+    }
+    return total
 }
 console.log(range(1,10))
 console.log(range(5,2))
-console.log(sum(1,10))
+console.log(sum(range(1,10)))
