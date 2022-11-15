35698bea7a9f035123b9218e4f6805f71a707aaa
z1OHH <oh.weerapat@gmail.com>
2022-11-15 21:30:45 +0700

    -

--- Section 2 - JavaScript/2.3_Thesumofarange.js
+++ Section 2 - JavaScript/2.3_Thesumofarange.js
@@ -1,4 +1,21 @@
 function range(start,end){
-
+let rangenum = []
+if(start < end){
+    for(let i = start; i <= end;i++){
+        rangenum.push(i)
+    }
 }
-console.log(range(1,5))
+else{
+    for(let i = start; i >= end;i--){
+        rangenum.push(i)
+    }
+}
+return rangenum
+}
+
+function sum(num){
+    
+}
+console.log(range(1,10))
+console.log(range(5,2))
+console.log(sum(1,10))
