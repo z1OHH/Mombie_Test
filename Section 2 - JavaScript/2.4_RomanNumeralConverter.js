0f1974ae707d65ede407720aa2742d9d271a0529
z1OHH <oh.weerapat@gmail.com>
2022-11-15 19:47:12 +0700

    #

---
+++ Section 2 - JavaScript/2.4_RomanNumeralConverter.js
@@ -0,0 +1,15 @@
+function convertToRoman(num){
+    let romanNum = ""
+    let romanLetter = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1}
+    for (let i in romanLetter) {
+        while (num >= romanLetter[i]) {
+            romanNum += i;
+            num -= romanLetter[i];
+        }
+    }
+    return romanNum
+}
+console.log(convertToRoman(16))
+console.log(convertToRoman(29))
+console.log(convertToRoman(99))
+console.log(convertToRoman(501))
