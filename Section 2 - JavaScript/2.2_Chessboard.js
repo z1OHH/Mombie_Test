a8e34b5d374f51063f64d57db1a9458690731149
z1OHH <oh.weerapat@gmail.com>
2022-11-15 22:06:43 +0700

    deleate test.js and apply prettier format for all

--- Section 2 - JavaScript/2.2_Chessboard.js
+++ Section 2 - JavaScript/2.2_Chessboard.js
@@ -1,10 +1,12 @@
-let cboard = "", height = 8, width = 8
+let cboard = "",
+  height = 8,
+  width = 8;
 for (let i = 1; i <= height; i++) {
   for (let j = 1; j <= width; j++) {
-    if ((i + j) % 2 == 0) { 
-        cboard += " ";
+    if ((i + j) % 2 == 0) {
+      cboard += " ";
     } else {
-        cboard += "#";
+      cboard += "#";
     }
   }
   cboard += "\n";
--- Section 2 - JavaScript/2.3_Thesumofarange.js
+++ Section 2 - JavaScript/2.3_Thesumofarange.js
@@ -1,26 +1,41 @@
-function range(start,end){
-    let rangenum = []
-    if(start < end){
-        for(let i = start; i <= end;i++){
-            rangenum.push(i)
-        }
-        return rangenum
+function range(start, end, step) {
+  let rangenum = [];
+  if (step == null) {
+    if (start < end) {
+      for (let i = start; i <= end; i++) {
+        rangenum.push(i);
+      }
+      return rangenum;
+    } else {
+      for (let i = start; i >= end; i--) {
+        rangenum.push(i);
+      }
+      return rangenum;
     }
-    else{
-        for(let i = start; i >= end;i--){
-            rangenum.push(i)
-        }
-        return rangenum
+  } else {
+    if (start < end) {
+      for (let i = start; i <= end; i += step) {
+        rangenum.push(i);
+      }
+      return rangenum;
+    } else {
+      for (let i = start; i >= end; i += step) {
+        rangenum.push(i);
+      }
+      return rangenum;
     }
+  }
 }
-    
-function sum(num){
-    total=0
-    for (let i = 1; i <= num.length; i++) {
-        total+=i
-    }
-    return total
+
+function sum(num) {
+  total = 0;
+  for (let i = 1; i <= num.length; i++) {
+    total += i;
+  }
+  return total;
 }
-console.log(range(1,10))
-console.log(range(5,2))
-console.log(sum(range(1,10)))
+console.log(range(1, 10));
+console.log(range(5, 2));
+console.log(range(1, 10, 2));
+console.log(range(8, 2, -3));
+console.log(sum(range(1, 10)));
