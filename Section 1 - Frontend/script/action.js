e14f6eb48a00c07c8198f9d876277d44277e1780
z1OHH <oh.weerapat@gmail.com>
2022-11-17 03:01:46 +0700

    Pretiear format

--- Section 1 - Frontend/script/action.js
+++ Section 1 - Frontend/script/action.js
@@ -1,43 +1,43 @@
 var fullImgBox = document.getElementById("fullImgBox");
 var fullImg = document.getElementById("fullImg");
 
-function openFullImg1(){
-    fullImgBox.style.display = "flex";
-    fullImg.src = "img/g1.jpg";
+function openFullImg1() {
+  fullImgBox.style.display = "flex";
+  fullImg.src = "img/g1.jpg";
 }
-function openFullImg2(){
-    fullImgBox.style.display = "flex";
-    fullImg.src = "img/g2.jpg";
+function openFullImg2() {
+  fullImgBox.style.display = "flex";
+  fullImg.src = "img/g2.jpg";
 }
-function openFullImg3(){
-    fullImgBox.style.display = "flex";
-    fullImg.src = "img/g3.jpg";
+function openFullImg3() {
+  fullImgBox.style.display = "flex";
+  fullImg.src = "img/g3.jpg";
 }
-function openFullImg4(){
-    fullImgBox.style.display = "flex";
-    fullImg.src = "img/g4.jpg";
+function openFullImg4() {
+  fullImgBox.style.display = "flex";
+  fullImg.src = "img/g4.jpg";
 }
-function openFullImg5(){
-    fullImgBox.style.display = "flex";
-    fullImg.src = "img/g5.jpg";
+function openFullImg5() {
+  fullImgBox.style.display = "flex";
+  fullImg.src = "img/g5.jpg";
 }
-function openFullImg6(){
-    fullImgBox.style.display = "flex";
-    fullImg.src = "img/g6.jpg";
+function openFullImg6() {
+  fullImgBox.style.display = "flex";
+  fullImg.src = "img/g6.jpg";
 }
-function openFullImg7(){
-    fullImgBox.style.display = "flex";
-    fullImg.src = "img/g7.jpg";
+function openFullImg7() {
+  fullImgBox.style.display = "flex";
+  fullImg.src = "img/g7.jpg";
 }
-function openFullImg8(){
-    fullImgBox.style.display = "flex";
-    fullImg.src = "img/g8.jpg";
+function openFullImg8() {
+  fullImgBox.style.display = "flex";
+  fullImg.src = "img/g8.jpg";
 }
-function openFullImg9(){
-    fullImgBox.style.display = "flex";
-    fullImg.src = "img/g9.jpg";
+function openFullImg9() {
+  fullImgBox.style.display = "flex";
+  fullImg.src = "img/g9.jpg";
 }
 
-function closeFullImg(){
-    fullImgBox.style.display = "none";
+function closeFullImg() {
+  fullImgBox.style.display = "none";
 }
